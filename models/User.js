37a1ccb17efc84f205ed95f1c76656c86b1ef1d9
Chuck Marlow <marlowchu@gmail.com>
2022-03-11 06:06:04 -0500

    server started

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const userSchema = require('./Assignment');
+// const userSchema = require('./Assignment');
 
 // Schema to create User model
 const userSchema = new Schema(
